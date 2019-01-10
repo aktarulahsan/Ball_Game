5cefbed1c22ec8a3a7af9f41dbffc3d4d47a4b4b
aktar <aktar@aktar-PC>
2019-01-10 17:37:57 +0600

    This is Ball Game  project .

---
+++ src/ballgame/Game.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ballgame;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author User
+ */
+public class Game extends JPanel implements Runnable {
+
+    Ball ball = new Ball(this);
+    Racquet racquet = new Racquet(this);
+    int speed=1;
+    
+    
+   
+    
+   
+
+    public int getScore() {
+        return speed - 1;
+    }
+
+    public Game() {
+        addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                racquet.keyReleased(e);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                racquet.keyPressed(e);
+            }
+        });
+        setFocusable(true);
+    }
+
+    private void move() {
+        ball.move();
+        racquet.move();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setColor(Color.RED);
+        ball.paint(g2d);
+        racquet.paint(g2d);
+
+        g2d.setColor(Color.GRAY);
+        g2d.setFont(new Font("Verdana", Font.BOLD, 30));
+        g2d.drawString(String.valueOf(getScore()), 10, 30);
+    }
+
+    public void gameOver() {
+        JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+        System.exit(0);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("Para");
+        Game game = new Game();
+        frame.add(game);
+        frame.setSize(300, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while (true) {
+            game.move();
+            game.repaint();
+            Thread.sleep(10);
+        }
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+}
---
+++ src/ballgame/Sound.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ballgame;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+/**
+ *
+ * @author User
+ */
+public class Sound {
+    public static final AudioClip BALL = Applet.newAudioClip(Sound.class.getResource("ball.wav"));
+}
